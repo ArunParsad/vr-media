53be65fdf6c07f4105b33a242928a1f45444bc50
Arun Mahato <ptech784@gmail.com>
2022-06-21 14:30:13 +0530

    home page done

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,45 @@
 import React from "react";
-import './style/Home.css';
-import './style/HomeMobile.css'
-import homeImg from './images/home.svg'
-import { NavLink } from 'react-router-dom';
+import "./style/Home.css";
+import "./style/HomeMobile.css";
+import homeImg from "./images/cartoon 17.png";
+import { NavLink } from "react-router-dom";
 import Button from "./Components/Button";
 
 const Home = () => {
   return (
     <>
-    <section className="home-section container p-2">
-      <section className="content">
-    <h1 className="text-wrap main-heading"><span style={{color:"rgb(68 68 68)"}}>We ensure and transform</span> <br /><span style={{color:"#00BAFF"}}>Business Growing</span></h1>
-      <section className="mobile-para">"Let's grow your business together."</section>
-      <section className="dekstop-button">
-      <NavLink to="/Services"><Button btnType="btn-hover color-9" className="get-started" text="Get Started" color="success" /></NavLink>
-      <NavLink to="/About"><Button btnType="btn-hover color-1" text="Learn More" color="info" /></NavLink>
-      </section>
-      </section>
-      <section className="img-section">
-        <img src={homeImg} className="img-fluid" />
-      </section>
-      <section className="mobile-button">
-      <NavLink to="/Services"><Button btnType="btn-hover color-9" className="get-started" text="Get Started" color="success" /></NavLink>
-      <NavLink to="/About"><Button btnType="btn-hover color-1" text="Learn More" color="info" /></NavLink>
-      </section>
-      </section>
+      <div className="container home-section">
+          <div className="content">
+            <h1 className="text-center text-wrap main-heading">
+              We ensure and transform <span>Business Growing</span> 🚀
+            </h1>
+            <p className="text-center">"Let's grow your business together."</p>
+            <div className="button d-flex justify-content-center">
+              <NavLink to="/Services">
+                <Button
+                  btnType="btn-hover color-9"
+                  className="get-started"
+                  text="Get Started"
+                  color="success"
+                />
+              </NavLink>
+              <NavLink to="/Services">
+                <Button
+                  btnType="btn-hover color-1"
+                  className="get-started"
+                  text="Learn more"
+                  color="success"
+                />
+              </NavLink>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center">
+            <img src={homeImg} className="img-fluid" />
+          </div>
+      </div>
     </>
   );
+
+
 };
 export default Home;
